96ef66b002acbdc803e97d595472be0df726d45b
MCshin00 <sinminc7@gmail.com>
2024-12-07 22:51:41 +0900

    feat: gateway에서 orders/create 요청만 로그를 표시하도록 필터 구현

--- gateway/src/main/java/com/spartacoding/msa/gateway/CustomLogGatewayFilterFactory.java
+++ gateway/src/main/java/com/spartacoding/msa/gateway/CustomLogGatewayFilterFactory.java
@@ -1,10 +1,13 @@
 package com.spartacoding.msa.gateway;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 /**
  * // TODO order-service에 특정 uri에만 적용될 log filter를 구현해주세요!
@@ -17,13 +20,32 @@ public class CustomLogGatewayFilterFactory extends AbstractGatewayFilterFactory<
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
+            logMessage(config, "Request Path : " + exchange.getRequest().getPath());
+            if (config.isLogHeaders()) {
+                logMessage(config, "Request Headers : " + exchange.getRequest().getHeaders());
+            }
 
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                logMessage(config, "Response Status : " + exchange.getResponse().getStatusCode());
+
+                if (config.isLogHeaders()) {
+                    logMessage(config, "Response Headers : " + exchange.getResponse().getHeaders());
+                }
+            }));
         };
     }
 
+    private void logMessage(Config config, String message) {
+        logger.info("{}, {}", config.getBaseMessage(), message);
+    }
+
+    @Data
+    @NoArgsConstructor
     public static class Config {
         // Configuration properties
         // TODO 로그가 출력되는 포맷을 설정하는 것도 좋을 것 같아요
+        private String baseMessage = "[CustomLogFilter 동작]";
+        private boolean logHeaders = false;
     }
 
     public CustomLogGatewayFilterFactory() {
--- gateway/src/main/java/com/spartacoding/msa/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/spartacoding/msa/gateway/GatewayConfig.java
@@ -1,32 +1,32 @@
-package com.spartacoding.msa.gateway;
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
+//package com.spartacoding.msa.gateway;
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
 // TODO Java-code로 라우팅을 설정해주세요! (여기에서 설정하면 application.yml에서는 설정하지 않아도 됩니다)
-// 아래는 예시입니다. 삭제 후 실제 코드 추가해주세요!
-public class GatewayConfig {
-
-    /**
-     * 예시입니다.
-     * CustomFilter 라는 가상의 클래스에 필요한 config 값을 설정하고, "sparta-service"라는 라우팅 룰을 추가하는 내용입니다.
-     * @param builder
-     * @param filter Custom filter 이름
-     * @return
-     */
-    @Bean
-    public RouteLocator customRoutes(RouteLocatorBuilder builder, CustomFilter filter) {
-        return builder.routes()
-                .route("sparta-service", r -> r.path("/sparta/**")
-                        .filters(f -> f.filter(filter.apply(new CustomFilter.Config() {{
-                            setClassType("sparta-class");
-                            setLectureName("eureka-gateway");
-                        }})))
-                        .uri("lb://sparta-service")
-                )
-                .build();
-    }
-}
+//// 아래는 예시입니다. 삭제 후 실제 코드 추가해주세요!
+//public class GatewayConfig {
+//
+//    /**
+//     * 예시입니다.
+//     * CustomFilter 라는 가상의 클래스에 필요한 config 값을 설정하고, "sparta-service"라는 라우팅 룰을 추가하는 내용입니다.
+//     * @param builder
+//     * @param filter Custom filter 이름
+//     * @return
+//     */
+//    @Bean
+//    public RouteLocator customRoutes(RouteLocatorBuilder builder, CustomFilter filter) {
+//        return builder.routes()
+//                .route("sparta-service", r -> r.path("/sparta/**")
+//                        .filters(f -> f.filter(filter.apply(new CustomFilter.Config() {{
+//                            setClassType("sparta-class");
+//                            setLectureName("eureka-gateway");
+//                        }})))
+//                        .uri("lb://sparta-service")
+//                )
+//                .build();
+//    }
+//}
